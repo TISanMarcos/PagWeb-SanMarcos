d9df768c8cdb370a1ffc508c1b6810d896a68ac2
Ale <ale@example.com>
2026-04-23 21:57:11 -0600

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/San-Marcos-Mascotas/',
   plugins: [react()],
   server: {
     proxy: {
